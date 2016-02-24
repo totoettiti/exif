113baeeaa80cf713d32d14fb8722a5d299fe76be
totoettiti <totoettiti@yopmail.com>
2016-02-24 14:08:16 +0100

    Update exif.js

--- exif.js
+++ exif.js
@@ -871,10 +871,21 @@ function imgCssGps(e){
 		var lonRef = EXIF.getTag(e, "GPSLongitudeRef");
 		var latRef = EXIF.getTag(e, "GPSLatitudeRef"); 
 		if (lon && lon!="") {	
+			var lonPretty = metatodecimal(parseInt(lon.split(",",3)[0]),parseInt(lon.split(",",3)[1]),parseInt(lon.split(",",3)[2]),lonRef);
 			alert(lon+"/"+lonRef+" : "+lat+"/"+latRef);
+			alert(lonPretty);
 			e.style.borderColor = 'red';
 			e.style.borderWidth = '10px';
 			e.style.borderStyle = 'solid';	
 		}
 	});
 };
+
+function metatodecimal(d,m,s,dir) {
+	var decimal = d+(m/60)+(s/3600);
+	if (dir=='S' || dir=='W') {
+		decimal *= -1;
+	}
+	return decimal;
+}
+}
